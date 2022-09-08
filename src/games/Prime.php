6714914a583c9192b8d69773e8c4dceaae30e199
Dmitry Semenov <kamstrim@mail.ru>
2022-09-08 19:30:09 +0300

    edit Prime.php

--- src/games/Prime.php
+++ src/games/Prime.php
@@ -4,6 +4,19 @@ namespace Php\Project_45\Games\Prime;
 
 use function Php\Project_45\Engine\game;
 
+function isPrime($num)
+{
+    if ($num < 2) {
+        return false;
+    }
+    for ($i = 2; $i <= $num / 2; $i++) {
+        if ($num % $i === 0) {
+          return false;
+        }
+      }
+    return true;
+}
+
 function gameStart()
 {
     $gameInstruction = 'Answer "yes" if given number is prime. Otherwise answer "no".';
@@ -14,7 +27,7 @@ function gameStart()
     for ($i = 0; $i < $roundsCount; $i++) {
         $gameNumber = rand(1, 30);
         $questions[$i] = "{$gameNumber}";
-        $correctAnswers[$i] = (gmp_prob_prime($gameNumber) === 2) ? 'yes' : 'no';
+        $correctAnswers[$i] = isPrime($gameNumber) ? 'yes' : 'no';
     }
     game($gameInstruction, $questions, $correctAnswers);
 }
